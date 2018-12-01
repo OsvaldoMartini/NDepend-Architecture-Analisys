fd5349523b95e34e7cc641b22d1106c550a55d96
Osvaldo.Martini <osvaldo.martini@gmail.com>
2018-12-01 15:13:55 +0000

    GeoCompanySales table

--- Geo-Localization/Project Files/Scripts/MySQL/SmarterASP_db_a418f0_martini.sql
+++ Geo-Localization/Project Files/Scripts/MySQL/SmarterASP_db_a418f0_martini.sql
@@ -1,8 +1,8 @@
 
-USE databaseName;
+USE db_a418f0_martini;
 /*
 CREATE USER IF NOT EXISTS 'admindb'@'localhost' IDENTIFIED BY 'admindb123';
-GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON databaseName.* TO 'admindb'@'localhost';
+GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON db_a418f0_martini.* TO 'admindb'@'localhost';
 GRANT ALTER ROUTINE, CREATE ROUTINE, EXECUTE ON *.* TO 'admindb'@'localhost' ;
 FLUSH PRIVILEGES;
 */
@@ -11,23 +11,23 @@ FLUSH PRIVILEGES;
 
 
 SET FOREIGN_KEY_CHECKS = 0;
-DROP TABLE IF EXISTS `databaseName`.`Employee`;
-DROP TABLE IF EXISTS `databaseName`.`CompanySales`;
-DROP TABLE IF EXISTS `databaseName`.`Company`;
-DROP TABLE IF EXISTS `databaseName`.`GeoLocalization`;
-DROP procedure IF EXISTS `PROC_DELETE_GEOLOCALIZATION`;
-DROP procedure IF EXISTS `PROC_DELETE_EMPLOYEE`;
-DROP procedure IF EXISTS `PROC_INSERT_GEOLOCALIZATION`;
-DROP procedure IF EXISTS `PROC_INSERT_EMPLOYEE`;
-DROP procedure IF EXISTS `PROC_UPDATE_EMPLOYEE`;
-DROP procedure IF EXISTS `PROC_UPDATE_GEOLOCALIZATION`;
+DROP TABLE IF EXISTS `db_a418f0_martini`.`GeoEmployee`;
+DROP TABLE IF EXISTS `db_a418f0_martini`.`GeoCompanySales`;
+DROP TABLE IF EXISTS `db_a418f0_martini`.`GeoCompany`;
+DROP TABLE IF EXISTS `db_a418f0_martini`.`GeoLocalization`;
+DROP procedure IF EXISTS `PROC_DELETE_Geolocalization`;
+DROP procedure IF EXISTS `PROC_DELETE_GeoEmployee`;
+DROP procedure IF EXISTS `PROC_INSERT_Geolocalization`;
+DROP procedure IF EXISTS `PROC_INSERT_GeoEmployee`;
+DROP procedure IF EXISTS `PROC_UPDATE_GeoEmployee`;
+DROP procedure IF EXISTS `PROC_UPDATE_Geolocalization`;
 SET FOREIGN_KEY_CHECKS = 1; 
 
 
 
 
-/****** Object:  Table Company    Script Date: 08/07/2017 09:07:02 ******/
-CREATE TABLE Company(
+/****** Object:  Table GeoCompany    Script Date: 08/07/2017 09:07:02 ******/
+CREATE TABLE GeoCompany(
 	CompanyID int AUTO_INCREMENT NOT NULL,
 	CompanyType varchar(100) NOT NULL,
 	Name varchar(100) NOT NULL,
@@ -39,7 +39,7 @@ CREATE TABLE Company(
 	WebSite varchar(255) NOT NULL,
 	Phone varchar(20) NOT NULL,
 	DateCreated datetime NULL,
-CONSTRAINT PK_Employee PRIMARY KEY CLUSTERED 
+CONSTRAINT PK_GeoCompany PRIMARY KEY CLUSTERED 
 (
 	CompanyID ASC
 )
@@ -47,8 +47,8 @@ CONSTRAINT PK_Employee PRIMARY KEY CLUSTERED
 ;
 
 
-/****** Object:  Table CompanySales    Script Date: 08/07/2017 09:07:02 ******/
-CREATE TABLE CompanySales(
+/****** Object:  Table GeoCompanySales    Script Date: 08/07/2017 09:07:02 ******/
+CREATE TABLE GeoCompanySales(
 	CompanySalesID int AUTO_INCREMENT NOT NULL,
 	CompanyID int NOT NULL,
 	SalesYear int NOT NULL,
@@ -63,9 +63,8 @@ CONSTRAINT PK_CompanySales PRIMARY KEY CLUSTERED
 ;
 
 
-
-/****** Object:  Table Employee Script Date: 08/07/2017 09:07:02 ******/
-CREATE TABLE Employee(
+/****** Object:  Table GeoEmployee Script Date: 08/07/2017 09:07:02 ******/
+CREATE TABLE GeoEmployee(
 	EmployeeID int AUTO_INCREMENT NOT NULL,
 	CompanyID int NOT NULL,
 	LastName varchar(30) NOT NULL,
@@ -75,7 +74,7 @@ CREATE TABLE Employee(
 	Password varchar(20) NOT NULL,
 	RoleID int NOT NULL,
 	DateCreated datetime NULL,
-CONSTRAINT PK_Employee PRIMARY KEY CLUSTERED 
+CONSTRAINT PK_GeoEmployee PRIMARY KEY CLUSTERED 
 (
 	EmployeeID ASC
 )
@@ -104,12 +103,12 @@ CREATE TABLE GeoLocalization(
 */
 
 
-ALTER TABLE Employee ADD CONSTRAINT FK_CompanyToEmployee 
-    FOREIGN KEY (CompanyID) REFERENCES Company(CompanyID);
+ALTER TABLE GeoEmployee ADD CONSTRAINT FK_GeoCompanyToGeoEmployee 
+    FOREIGN KEY (CompanyID) REFERENCES GeoCompany(CompanyID);
 
 
-ALTER TABLE GeoLocalization ADD CONSTRAINT FK_GeoLocalizationToEmployee
-    FOREIGN KEY (EmployeeID) REFERENCES Employee(EmployeeID);
+ALTER TABLE GeoLocalization ADD CONSTRAINT FK_GeoLocalizationToGeoEmployee
+    FOREIGN KEY (EmployeeID) REFERENCES GeoEmployee(EmployeeID);
 
 /*
 # ---------------------------------------------------------------------- #
@@ -118,20 +117,20 @@ ALTER TABLE GeoLocalization ADD CONSTRAINT FK_GeoLocalizationToEmployee
 */
 
 
-INSERT INTO `Company` (`CompanyType`,`Name`,`Address`,`PostCode`,`State`,`Country`,`Email`,`WebSite`,`Phone`,`DateCreated`) 
+INSERT INTO `GeoCompany` (`CompanyType`,`Name`,`Address`,`PostCode`,`State`,`Country`,`Email`,`WebSite`,`Phone`,`DateCreated`) 
 VALUES('IT Services','WServices','W. Services Global Headquarters Receptionist','W1W','London','United Kingdom','osvaldo.martini@gmail.com  ','osvaldo.martini@gmail.com  ', '+44 (0)7951 144 116', CURRENT_TIMESTAMP());
 
-INSERT INTO `Employee` (`CompanyID`,`LastName`,`FirstName`,`UserName`,`Email`,`Password`,`RoleID`,`DateCreated`) VALUES(1, 'admin','Adm','Admin','admin@gmail.com','admin', 1, CURRENT_TIMESTAMP());
-INSERT INTO `Employee` (`CompanyID`,`LastName`,`FirstName`,`UserName`,`Email`,`Password`,`RoleID`,`DateCreated`) VALUES(1, 'Martini','Osvaldo','Omartini','osvaldo.martini@gmail.com','martini', 1, CURRENT_TIMESTAMP());
+INSERT INTO `GeoEmployee` (`CompanyID`,`LastName`,`FirstName`,`UserName`,`Email`,`Password`,`RoleID`,`DateCreated`) VALUES(1, 'admin','Adm','Admin','admin@gmail.com','admin', 1, CURRENT_TIMESTAMP());
+INSERT INTO `GeoEmployee` (`CompanyID`,`LastName`,`FirstName`,`UserName`,`Email`,`Password`,`RoleID`,`DateCreated`) VALUES(1, 'Martini','Osvaldo','Omartini','osvaldo.martini@gmail.com','martini', 1, CURRENT_TIMESTAMP());
 
 INSERT INTO `GeoLocalization` (`EmployeeID`,`LocalName`,`Lat`,`Lng`,`DateCreated`) VALUES(1,'Liverpool Museum','-2.979919','51.521437', CURRENT_TIMESTAMP());
 INSERT INTO `GeoLocalization` (`EmployeeID`,`LocalName`,`Lat`,`Lng`,`DateCreated`) VALUES(1,'Merseyside Maritime Museum','-0.081437','51.518952', CURRENT_TIMESTAMP());
 INSERT INTO `GeoLocalization` (`EmployeeID`,`LocalName`,`Lat`,`Lng`,`DateCreated`) VALUES(1,'Walker Art Gallery','-0.143975','51.521831', CURRENT_TIMESTAMP());
 INSERT INTO `GeoLocalization` (`EmployeeID`,`LocalName`,`Lat`,`Lng`,`DateCreated`) VALUES(1,'National Conservation Centre','-2.984683','53.407511', CURRENT_TIMESTAMP());
 
-/****** Object:  StoredProcedure PROC_UPDATE_EMPLOYEE    Script Date: 08/22/2017 07:02:44 ******/
+/****** Object:  StoredProcedure PROC_UPDATE_GeoEmployee    Script Date: 08/22/2017 07:02:44 ******/
 DELIMITER $$
-CREATE DEFINER=`databaseName`@`%` PROCEDURE `PROC_UPDATE_EMPLOYEE`(
+CREATE  PROCEDURE `PROC_UPDATE_GeoEmployee`(
 in P_CompanyID int,
 in P_EmployeeID int,
 in P_LastName varchar(30),
@@ -152,7 +151,7 @@ declare exit handler for SQLEXCEPTION
   
 start transaction;
     
-         UPDATE Employee
+         UPDATE GeoEmployee
                         SET LastName = P_LastName
                          ,FirstName = P_FirstName
                          ,UserName = P_UserName
@@ -172,9 +171,9 @@ DELIMITER ;
 
 
 
-/****** Object:  StoredProcedure PROC_INSERT_EMPLOYEE    Script Date: 08/22/2017 07:02:32 ******/
+/****** Object:  StoredProcedure PROC_INSERT_GeoEmployee    Script Date: 08/22/2017 07:02:32 ******/
 DELIMITER $$
-CREATE DEFINER=`databaseName`@`%` PROCEDURE `PROC_INSERT_EMPLOYEE`(
+CREATE  PROCEDURE `PROC_INSERT_GeoEmployee`(
 in P_CompanyID int,
 in P_LastName varchar(20),
 in P_FirstName varchar(30),
@@ -194,7 +193,7 @@ declare exit handler for SQLEXCEPTION
   
 start transaction;
     
-INSERT INTO Employee 
+INSERT INTO GeoEmployee 
 			(CompanyID
 			,FirstName 
            ,LastName
@@ -226,9 +225,9 @@ DELIMITER ;
 
 
 
-/****** Object:  StoredProcedure PROC_DELETE_EMPLOYEE    Script Date: 09/18/2017 04:44:44 ******/
+/****** Object:  StoredProcedure PROC_DELETE_GeoEmployee    Script Date: 09/18/2017 04:44:44 ******/
 DELIMITER $$
-CREATE DEFINER=`databaseName`@`%` PROCEDURE `PROC_DELETE_EMPLOYEE`(
+CREATE  PROCEDURE `PROC_DELETE_GeoEmployee`(
 in P_CompanyID int,
 in P_EmployeeID int,
 out P_Return_Message varchar(500) 
@@ -244,7 +243,7 @@ set P_Return_Message = 'Operation Not Allowed!';
 start transaction;
 
 
-	DELETE FROM Employee 
+	DELETE FROM GeoEmployee 
 WHERE
     CompanyID = P_CompanyID and
     EmployeeID = P_EmployeeID;
@@ -257,9 +256,9 @@ END$$
 DELIMITER ;
 
 
-/****** Object:  StoredProcedure PROC_UPDATE_GEOLOCALIZATION    Script Date: 08/22/2017 07:02:35 ******/
+/****** Object:  StoredProcedure PROC_UPDATE_Geolocalization    Script Date: 08/22/2017 07:02:35 ******/
 DELIMITER $$
-CREATE DEFINER=`databaseName`@`%` PROCEDURE `PROC_UPDATE_GEOLOCALIZATION` (
+CREATE  PROCEDURE `PROC_UPDATE_Geolocalization` (
 in P_GeoLocalizationID int,
 in P_EmployeeID int ,
 in P_LocalName varchar(100) ,
@@ -294,9 +293,9 @@ END$$
 DELIMITER ;
 
 
-/****** Object:  StoredProcedure PROC_INSERT_GEOLOCALIZATION    Script Date: 08/22/2017 07:02:22 ******/
+/****** Object:  StoredProcedure PROC_INSERT_Geolocalization    Script Date: 08/22/2017 07:02:22 ******/
 DELIMITER $$
-CREATE DEFINER=`databaseName`@`%` PROCEDURE `PROC_INSERT_GEOLOCALIZATION` (
+CREATE  PROCEDURE `PROC_INSERT_Geolocalization` (
 in P_EmployeeID int,
 in P_LocalName varchar(100),
 in P_Long varchar(30) ,
@@ -342,9 +341,9 @@ DELIMITER ;
 
 
 
-/****** Object:  StoredProcedure PROC_DELETE_GEOLOCALIZATION    Script Date: 09/18/2017 04:46:27 ******/
+/****** Object:  StoredProcedure PROC_DELETE_Geolocalization    Script Date: 09/18/2017 04:46:27 ******/
 DELIMITER $$
-CREATE DEFINER=`databaseName`@`%` PROCEDURE `PROC_DELETE_GEOLOCALIZATION`(
+CREATE  PROCEDURE `PROC_DELETE_Geolocalization`(
 in P_EmployeeID int,
 in P_GeoLocalizationID int,
 out P_Return_Message varchar(500)
